9d9c355d581ddb9a821ca53051e490f63ac5c52f
TheBrendinator <b.scott2@rgu.ac.uk>
2026-02-28 15:57:58 +0000

    feat: add array of colors

--- jon_ledhand/hat_stuff.py
+++ jon_ledhand/hat_stuff.py
@@ -1,9 +1,12 @@
 # This file will convert images to the sense hat displays as well as provide the ability to sync and connect across multiple sense hats
 
+from typing import TypedDict
+import itertools
 
 SINGLE_PI_RES_X: int = 8
 SINGLE_PI_RES_Y: int = 8
-node_list: list[dict[str,int]] = []
+node = TypedDict("node", {"x": int, "y": int, "colors": list[list[int]]})
+node_list: list[node] = []
 image_res: list[int] = [SINGLE_PI_RES_X,SINGLE_PI_RES_Y]
 
 
@@ -12,12 +15,14 @@ def example_func(print_string: str):
 
 
 def create_node_list(pi_count_x: int, pi_count_y: int):
+        colors: list[list[int]] = list(itertools.repeat([255,255,255], SINGLE_PI_RES_X * SINGLE_PI_RES_Y))
         for i in range(pi_count_x):
                 for j in range(pi_count_y):
-                        node: dict[str,int] = {
+                        temp_node: node = {
                                 "x": i,
                                 "y": j,
+                                "colors": colors
                         }
-                        node_list.append(node)
+                        node_list.append(temp_node)
         image_res = [(pi_count_x * SINGLE_PI_RES_X) + (pi_count_y * SINGLE_PI_RES_X)]
         return node_list, image_res
